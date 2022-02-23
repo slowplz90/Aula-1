a766d5a39c718e8da033630ac7ed58b637d1438c
slowplz90 <74824817+slowplz90@users.noreply.github.com>
2022-02-23 16:39:29 +0000

    Exercicio2

---
+++ exc2/Exc2/Exc2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string alunos = "alunos";
+string bv = "bem vindos";
+
+Console.WriteLine("Caros {0} sejam {1} ao C#", alunos, bv);
+
