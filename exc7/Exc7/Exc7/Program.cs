201982e22673d448134fe39153ce30695ad2a12f
slowplz90 <74824817+slowplz90@users.noreply.github.com>
2022-02-24 15:11:10 +0000

    Exercicios

---
+++ exc7/Exc7/Exc7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Resultado de 3^5: {0}", Math.Pow(3,5));
+Console.WriteLine("Raiz quadrada de 125: {0}", Math.Sqrt(125));
+decimal Valor = 123.456M;
+Console.WriteLine("Arredondar " + Valor + " a 0 casas decimais");
+Valor = decimal.Round(Valor, 0);
+Console.WriteLine("Valor arredondado a 0 casas decimais: {0}", Valor);
+Console.WriteLine("Seno de 30: {0}", Math.Sin(30));
