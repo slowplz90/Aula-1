526a24a20d678c71e93bb6f3f6ace509a9fe72b1
slowplz90 <74824817+slowplz90@users.noreply.github.com>
2022-02-24 15:16:16 +0000

    Exercicios

---
+++ exc8/Exc8/Exc8/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("4==5 {0}", 4==5);
+Console.WriteLine("4!=6 {0}", 4!=6);
+Console.WriteLine("4>5 {0}", 4>5);
+Console.WriteLine("4<5 e 6>10 {0}", 4<5 && 6>10);
+Console.WriteLine("40<50 ou 60>90 {0}", 40<50 || 60>90);
+Console.WriteLine("NOT(40<50 ou 60>90) {0}", !(40<50 || 60>90));
