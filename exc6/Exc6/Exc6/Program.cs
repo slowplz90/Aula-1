02e4a6dec1a91c393f8e473833f9b813a7c184fa
slowplz90 <74824817+slowplz90@users.noreply.github.com>
2022-02-24 14:53:02 +0000

    Exercicio 6

---
+++ exc6/Exc6/Exc6/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Resultado de 4+3x(15-2): {0}", 4+3 * (15-2));
+Console.WriteLine("Resultado de 40/9: {0}", 40/9);
+Console.WriteLine("Resto da divisão de 40 por 3: {0}", 40%3);
